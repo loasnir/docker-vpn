5513338479e4813b15104299eaa2030004cd6878
鈴木徹 <tree.suzuki@yahoo.com>
2017-12-17 14:01:53 +0900

    modify firewall port setting

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,7 +3,7 @@ yum install -y docker docker-compose
 systemctl start docker
 systemctl enable docker
 
-firewall-cmd --zone=public --add-port=80/tcp --permanent
+firewall-cmd --zone=public --add-service=http --permanent
 firewall-cmd --zone=public --add-port=443/tcp --permanent
 firewall-cmd --zone=public --add-port=1194/tcp --permanent
 firewall-cmd --zone=public --add-port=1194/udp --permanent
